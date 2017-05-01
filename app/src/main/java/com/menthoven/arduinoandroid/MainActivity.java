4bc0a555bc0dffa49eb9cd0118a8c538ed7000d5
Bilal rashid <bilal.rashid@hypernymbiz.com>
2017-05-02 00:04:24 +0500

    ui improved and permissions added

--- app/src/main/java/com/menthoven/arduinoandroid/MainActivity.java
+++ app/src/main/java/com/menthoven/arduinoandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.menthoven.arduinoandroid;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -7,9 +8,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     BluetoothDevicesAdapter bluetoothDevicesAdapter;
 
+    public static final int MY_LOCATION_PERMISSION_REQUEST_CODE = 1;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
     @Bind(R.id.devices_list_view)
@@ -42,6 +48,33 @@ public class MainActivity extends AppCompatActivity {
     CoordinatorLayout coordinatorLayout;
 
     @OnClick(R.id.search_button) void search() {
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_LOCATION_PERMISSION_REQUEST_CODE);
+            }
+            return;
+        }
+
+        if (bluetoothAdapter.isEnabled()) {
+            // Bluetooth enabled
+            startSearching();
+        } else {
+
+            enableBluetooth();
+        }
+    }
+
+    public void tryagain(){
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_LOCATION_PERMISSION_REQUEST_CODE);
+            }
+            return;
+        }
 
         if (bluetoothAdapter.isEnabled()) {
             // Bluetooth enabled
@@ -159,6 +192,40 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_LOCATION_PERMISSION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            ) {
+                        if (bluetoothAdapter.isEnabled()) {
+                            // Bluetooth enabled
+                            startSearching();
+                        } else {
+
+                            enableBluetooth();
+                        }
+                    }
+                } else {
+                    Snackbar.make(coordinatorLayout, "Permission Denied", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Try Again", new View.OnClickListener() {
+                                @Override public void onClick(View v) {
+                                    tryagain();
+                                }
+                            }).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     private void startSearching() {
